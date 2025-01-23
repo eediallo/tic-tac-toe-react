57e214213e32c07d51873141234dba5679db8625
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-01-23 10:36:42 +0000

    handle click

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,29 @@
+import { useState } from "react";
+
 export default function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [edit, setEdit] = useState("Edit");
+
+  function handleClick() {
+    if (!isEditing) {
+      setIsEditing(true);
+      setEdit("Save");
+    }
+  }
+
+  let playerName = (
+    <span className={isEditing ? "player-name" : ""}>{name}</span>
+  );
+  if (isEditing) {
+    playerName = <input />;
+  }
+
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>{" "}
+        {playerName}
         <span className="player-symbol">{symbol}</span>
-        <button>Edit</button>
+        <button onClick={handleClick}>{edit}</button>
       </span>
     </li>
   );
