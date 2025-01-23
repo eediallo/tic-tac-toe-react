5e08e9793cc54f0156a66b8e90f98528fb95f5c4
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-01-23 12:58:22 +0000

    update the game board

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 let initialGameBoard = [
   [null, null, null],
   [null, null, null],
@@ -5,13 +7,27 @@ let initialGameBoard = [
 ];
 
 export default function GameBoard() {
+  const [boardGame, setGameBoard] = useState(initialGameBoard);
+
+  function handleSelectSquare(rowIndex, colIndex) {
+    setGameBoard((prevBoardGame) => {
+      const updatedGameBoard = [
+        ...prevBoardGame.map((innerArray) => [...innerArray]),
+      ];
+
+      updatedGameBoard[rowIndex][colIndex] = "X";
+      return updatedGameBoard;
+    });
+  }
   return (
     <ol id="game-board">
-      {initialGameBoard.map((row, rowIndex) => (
+      {boardGame.map((row, rowIndex) => (
         <ol key={rowIndex}>
           {row.map((playerSymbol, colIndex) => (
             <li key={colIndex}>
-              <button>{playerSymbol}</button>
+              <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
+                {playerSymbol}
+              </button>
             </li>
           ))}
         </ol>
