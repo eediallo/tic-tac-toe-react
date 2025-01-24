d3bd782231ee644643296ecf33fdfd98a5d0a955
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-01-24 11:29:08 +0000

    ensure button is only clicked once

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -33,7 +33,10 @@ export default function GameBoard({ onSelectSquare, turns }) {
         <ol key={rowIndex}>
           {row.map((playerSymbol, colIndex) => (
             <li key={colIndex}>
-              <button onClick={() => onSelectSquare(rowIndex, colIndex)}>
+              <button
+                onClick={() => onSelectSquare(rowIndex, colIndex)}
+                disabled={playerSymbol !== null}
+              >
                 {playerSymbol}
               </button>
             </li>
