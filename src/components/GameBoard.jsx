15506debe5ad941ef67873938f45d8ae397d16d5
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-01-24 11:54:28 +0000

    gets rid of unused code

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,35 +1,7 @@
-import { useState } from "react";
-
-let initialGameBoard = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null],
-];
-
-export default function GameBoard({ onSelectSquare, turns }) {
-  let gameBoard = initialGameBoard;
-  for (const turn of turns) {
-    const { square, player } = turn;
-    const { row, col } = square;
-
-    gameBoard[row][col] = player;
-  }
-  // const [boardGame, setGameBoard] = useState(initialGameBoard);
-
-  // function handleSelectSquare(rowIndex, colIndex) {
-  //   setGameBoard((prevBoardGame) => {
-  //     const updatedGameBoard = [
-  //       ...prevBoardGame.map((innerArray) => [...innerArray]),
-  //     ];
-
-  //     updatedGameBoard[rowIndex][colIndex] = activePlayerSymbol;
-  //     return updatedGameBoard;
-  //   });
-  //   onSelectSquare();
-  // }
+export default function GameBoard({ onSelectSquare, board }) {
   return (
     <ol id="game-board">
-      {gameBoard.map((row, rowIndex) => (
+      {board.map((row, rowIndex) => (
         <ol key={rowIndex}>
           {row.map((playerSymbol, colIndex) => (
             <li key={colIndex}>
